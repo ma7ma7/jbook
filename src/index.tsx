7b005b672df0f865d4436830c59687cc4c6ed2fb
ma7ma7 <mahmoudamane5@gmail.comm>
2021-11-01 17:21:29 +0100

    Start transpilnig - Give modern Javascript syntaxt to text Area - Code transpiled to modern JS Code

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,35 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import ReactDOM from 'react-dom';
 import * as esbuild from 'esbuild-wasm';
 
 export const App = () => {
+  const ref = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
-  const onClick = () => console.log(code);
-
   const startService = async () => {
-    const service = await esbuild.startService({
+    ref.current = await esbuild.startService({
       worker: true,
       wasmURL: '/esbuild.wasm',
     });
-
-    console.log(service);
   };
 
   useEffect(() => {
     startService();
   }, []);
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+
+    const result = await ref.current.transform(input, {
+      loader: 'jsx',
+      target: 'es2015',
+    });
+
+    setCode(result.code);
+  };
 
   return (
     <div>
